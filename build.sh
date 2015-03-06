d5db0002ed9c9e359c0eff3e230865c1f066ccd7
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-03-06 12:28:38 -0500

    test

--- build.sh
+++ build.sh
@@ -13,19 +13,18 @@ set -e
 jekyll build
 
 # cleanup
-# rm -rf ../eshepelyuk.github.io.master
+rm -rf ../ramp-docs-dist
 
 #clone `master' branch of the repository using encrypted GH_TOKEN for authentification
-# git clone https://${GH_TOKEN}@github.com/eshepelyuk/eshepelyuk.github.io.git ../eshepelyuk.github.io.master
+git clone -b test/autoRelease https://${GH_TOKEN}@github.com/RAMP-PCAR/ramp-pcar-docs ../ramp-docs-dist
 
 # copy generated HTML site to `master' branch
-# cp -R _site/* ../eshepelyuk.github.io.master
+cp -R _site/* ../ramp-docs-dist
 
 # commit and push generated content to `master' branch
 # since repository was cloned in write mode with token auth - we can push there
-# cd ../eshepelyuk.github.io.master
-# git config user.email "eshepelyuk@gmail.com"
-# git config user.name "Evgeny Shepelyuk"
-# git add -A .
-# git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-# git push --quiet origin master > /dev/null 2>&1 
+cd ../ramp-docs-dist
+git config user.name "ramp-pcar-bot"
+git add -A .
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push --quiet origin doctest > /dev/null 2>&1 
