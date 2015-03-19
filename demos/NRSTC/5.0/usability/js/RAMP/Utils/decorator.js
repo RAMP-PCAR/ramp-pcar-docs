2b5443fb162386a4688c21c79e9b5b2cc659cc95
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-03-19 08:30:58 -0400

    chore(grunt): remove demo files from docs repo

--- demos/NRSTC/5.0/usability/js/RAMP/Utils/decorator.js
+++
@@ -1,5 +0,0 @@
-/*! ramp-theme-usability 13-02-2015 19:37:32 : v. 5.0.1 
- * 
- * RAMP GIS viewer - Elk; Sample of an implementation of RAMP with Usability Theme 
- **/
-define([],function(){"use strict";return{getFindFcn:function(a,b){function c(c){return a(b,c)}return c}}});
--- demos/NRSTC/5.0/usability/js/RAMP/Utils/functionMangler.js
+++
@@ -1,5 +0,0 @@
-/*! ramp-theme-usability 13-02-2015 19:37:32 : v. 5.0.1 
- * 
- * RAMP GIS viewer - Elk; Sample of an implementation of RAMP with Usability Theme 
- **/
-define(["dojo/topic","dojo/_base/lang","dojo/on","utils/util"],function(a,b,c,d){"use strict";function e(){var e=c;c=function(a,c,f,g){return d.isUndefined(g)?e(a,c,f):e(a,c,b.hitch(g,f))};var f=a.subscribe;a.subscribe=function(a,b){return d.isUndefined(a)?void 0:f(a,b)}}return{load:function(a,b,c){e(),c()}}});
