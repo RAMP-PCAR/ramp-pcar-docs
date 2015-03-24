5a9dc9840cd02015baca82624f0600e35f0c4362
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-03-24 09:23:23 -0400

    chore(grunt): update live repo url

--- build.sh
+++ build.sh
@@ -16,10 +16,10 @@ targetBranch="gh-pages"
 targetRepo="https://${GH_TOKEN}@github.com/RAMP-PCAR/ramp-pcar-docs"
 commitMessage="chore(update): RAMP Docs edge Travis build #$TRAVIS_BUILD_NUMBER"
 
-# push to live repo if master branch and there is a tag
-if [ $TRAVIS_BRANCH == "test/autoRelease" ]; then
-    targetBranch="tmaster"
-    targetRepo="https://${GH_TOKEN}@github.com/RAMP-PCAR/ramp-pcar-docs"
+# push to live repo if master branch
+if [ $TRAVIS_BRANCH == "master" ]; then
+    targetBranch="master"
+    targetRepo="https://${GH_TOKEN}@github.com/RAMP-PCAR/RAMP-PCAR.github.io"
     commitMessage="chore(release): RAMP Docs live Travis build #$TRAVIS_BUILD_NUMBER" 
 fi
 
