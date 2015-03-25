0d5076f2fa8a8fed392f2dcdeb5244bcf4cad81b
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-03-25 09:04:53 -0400

    feat(site-templates): add "edge" ribbon
    Makes it easier to distinguish between "live" and "edge" sites.

---
+++ assets/themes/gc/js/ribbon.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () {
+    if (location.pathname.indexOf("/ramp-pcar-docs/") !== -1) {
+        $(body).append('<img src="/assets/images/ramp_docs_edge.png" style="position: absolute; top: 0; right: 0;">');
+    }
+});
